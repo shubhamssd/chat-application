d3a6fcc1e5865a1a12c036afee291e48fe956265
Shubham Dhanvate <shubhamssd10@gmail.com>
2025-03-16 16:24:04 +0530

    Project Configuration

---
+++ ChatApp/chat-app/src/main/java/com/shubhamssd/chat_app/ChatAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.shubhamssd.chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApiApplication.class, args);
+	}
+
+}
---
+++ ChatApp/chat-app/src/test/java/com/shubhamssd/chat_app/ChatAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhamssd.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
